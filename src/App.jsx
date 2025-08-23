ddc34c2c3c0821b39e63c2d221ba99b47dd33102
Ravikanth <ravikanthanu55@gmail.com>
2025-08-24 00:05:05 +0530

    added Boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+
+
+
+
+function App(){
+
+
+  return(
+
+    <>
+    
+    <div className="flex text-4xl text-center mx-auto mt-24">
+      <h1 className="mx-auto mt-50 text-blue-600 font-bold">This is a Home page..</h1>
+    </div>
+    
+    </>
+  )
+}
+
+
+
+export default App;
